7cec5d7acaf0fde67f05d2e07ad7907ca469311c
Valeria Suarez Moreno <valesmuniversidad@gmail.com>
2026-05-02 18:08:04 -0500

    1

--- song.py
+++ song.py
@@ -1,1 +1,10 @@
 # Write your code here!
+class Song:
+    def __init__(self, name, artist, length):
+        self.name = name
+        self.artist = artist
+        self.length = length
+    def get_length_in_seconds(self):
+        return self.length * 60
+my_song = Song("tv off", "Kendrick Lamar", 3.7)
+print(my_song.get_length_in_seconds())
