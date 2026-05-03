0f36447679d467e0e91e7e2c89cbff2325af8b5d
Valeria Suarez Moreno <valesmuniversidad@gmail.com>
2026-05-02 21:21:38 -0500

    9

--- car_utils.py
+++ car_utils.py
@@ -1,5 +1,4 @@
 # FREEZE CODE BEGIN
-from car_utils import create_car_from_input, display_cars
 from car import *
 
 def create_car_from_input():
@@ -14,41 +13,3 @@ def display_cars(car_dict):
   for car in car_dict.values():
      print(car)
 # FREEZE CODE END
-cars = {}
-while True:
-    opcion = input("""
-                  1. Add car
-                  2. View cars
-                  3. Drive car
-                  4. Paint car
-                  5. Exit""")
-    if opcion == "1":
-           car = create_car_from_input()
-           cars[car.car_id] = car
-           print(car)
-           print("Car added.")
-   
-    elif opcion == "2":
-           display_cars(cars)
-   
-    elif opcion == "3":
-           car_id = input()
-           miles = float(input())
-   
-           if car_id in cars:
-               cars[car_id].drive(miles)
-               print("Mileage updated.")
-               print(cars[car_id])
-   
-    elif opcion == "4":
-           car_id = input()
-           new_color = input()
-   
-           if car_id in cars:
-               cars[car_id].change_color(new_color)
-               print("Color updated.")
-               print(cars[car_id])
-   
-    elif opcion == "5":
-           print("Goodbye!")
-           break
--- overall.py
+++ overall.py
@@ -1,4 +1,5 @@
 # TODO: Import modules
+from car_utils import create_car_from_input, display_cars
 def main():
     cars = {}  # Dictionary to store cars with car_id as key and car objects as values
 
@@ -15,24 +16,37 @@ def main():
         if choice == '1':
           """TODO: Call the appropriate function from utils.py to create 
           the car, add it to the dictionary, and print the car."""
+          car = create_car_from_input()
+          cars[car.car_id] = car
+          print(car)
+          print("Car added.")
 
 
         elif choice == '2':
           """TODO: Call the appropriate function from utils.py to display
           all the cars in the cars dictionary."""
+          display_cars(cars)
 
         elif choice == '3':
           car_id = input("Enter the car ID to drive:\n")
           miles = float(input("How many miles to drive?\n"))
           """TODO: Look up the car in the dictionary, call the appropriate
           class method to increase the mileage of the car, and print the car."""
-          
+          if car_id in cars:
+                      cars[car_id].drive(miles)
+                      print("Mileage updated.")
+                      print(cars[car_id])
+
         elif choice == '4':
           car_id = input("Enter the car ID to paint:\n")
           new_color = input("Enter the new color:\n")
           """TODO: Look up the car in the dictionary, call the appropriate
           class method to change the color of the car, and print the car."""
-
+          if car_id in cars:
+                      cars[car_id].change_color(new_color)
+                      print("Color updated.")
+                      print(cars[car_id])
+        
         elif choice == '5':
             print("Goodbye!")
             break
