914d470e0623ec3ba233cc8364c14357c24182e3
Valeria Suarez Moreno <valesmuniversidad@gmail.com>
2026-05-02 21:35:06 -0500

    11

--- bank_account.py
+++ bank_account.py
@@ -1,1 +1,19 @@
 # bank_account.py
+class BankAccount:
+    def __init__(self, account_number, balance=0.0):
+        self.account_number= account_number
+        self.balance= balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+                return -1
+        else:
+            self.balance -= amount
+            return 0
+        
+    def __str__(self):
+        last_digits = str(self.account_number)[-2:]
+        return f"Account Number: **{last_digits}\nCurrent Balance: {self.balance:.2f}"
